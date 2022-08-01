43f480d6442f60c1afe7dcf0d1381eafbfa99f5d
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 14:03:41 +0300

    Prime numbers
    internship

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrimeNumber(int number)
+            {
+                for (int i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+
+                }
+                return true;
+            }
+
+
+            Console.WriteLine("Please, give me a number...");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                if (isPrimeNumber(number))
+                {
+                    Console.WriteLine("This number is a prime number.");
+                }
+                else
+                {
+                    Console.WriteLine("This number is not a prime number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("your input is not a number!");
+            }
+            Console.ReadLine();
+
+        }
+        
+    }
+}
